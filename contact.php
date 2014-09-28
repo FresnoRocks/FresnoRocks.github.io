275a5fd45cea1cbb64f08d18e288fc164e32c463
James Kinler <james193722@yahoo.com>
2014-09-28 13:33:38 -0700

    Couldnt get my contact form to work

--- contact.php
+++ contact.php
@@ -1,15 +1,29 @@
 <?php
-$name = $_POST['fname'];
-$email = $_POST['lname'];
-$message = $_POST['email'];
 
-//Sending Email to form owner
-$header = "From: $email\n"
-. "Reply-To: $email\n";
-$subject = "Submission From My Form";
-$email_to = "james193722@yahoo.com";
-$message = "name: $name . $email . $message\n"
-. "email: $email\n";
-mail($email_to, $subject ,$message ,$header ) ;
+/* Set e-mail recipient */
+$myemail = "james193722@yahoo.com";
 
-?>
+
+$name = check_input($_POST['inputName'], "Your Name");
+$email = check_input($_POST['inputEmail'], "Your E-mail Address");
+$message = check_input($_POST['inputComments'], "Your Message");
+
+/* Let's prepare the message for the e-mail */
+
+$subject = "Someone has sent you a message";
+
+$message = "
+
+Someone has sent you a message using your contac form:
+
+Name: $name
+Email: $email
+Subject: $subject
+
+Message:
+$message
+
+";
+
+/* Send the message using mail() function */
+mail($myemail, $subject, $message);
